b8a2c5f3b179f6b04d492e6317a2df62319ea7fb
jewlexx <61040004+jewlexx@users.noreply.github.com>
2024-10-25 18:48:48 +1100

    implemented serialization

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,16 @@
 mod parsing;
+mod serializing;
 
-use std::path::PathBuf;
+use std::{path::PathBuf, str::FromStr};
+
+#[derive(Debug, thiserror::Error)]
+pub enum Error {
+    #[error("{0}")]
+    ParsingError(#[from] parsing::Error),
+
+    #[error("{0}")]
+    SerializationError(#[from] serializing::Error),
+}
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct Shim {
@@ -20,4 +30,8 @@ impl Shim {
     pub fn args(&self) -> &[String] {
         &self.args
     }
+}
+
+pub fn from_str(s: &str) -> Result<Shim, Error> {
+    Ok(Shim::from_str(s)?)
 }
---
+++ src/serializing.rs
@@ -0,0 +1,44 @@
+use std::{borrow::Cow, fmt::Display};
+
+use crate::Shim;
+
+#[derive(Debug, thiserror::Error)]
+pub enum Error {}
+
+impl Display for Shim {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let lines = Line::from_shim(self);
+
+        for line in lines {
+            Display::fmt(&line, f)?;
+        }
+
+        Ok(())
+    }
+}
+
+struct Line<'a> {
+    key: &'static str,
+    value: Cow<'a, str>,
+}
+
+impl<'a> Line<'a> {
+    fn from_shim(shim: &'a Shim) -> [Self; 2] {
+        [
+            Line {
+                key: "path",
+                value: shim.path().to_string_lossy(),
+            },
+            Line {
+                key: "args",
+                value: Cow::Owned(shim.args().join(" ")),
+            },
+        ]
+    }
+}
+
+impl<'a> Display for Line<'a> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{} = {}", self.key, self.value)
+    }
+}
