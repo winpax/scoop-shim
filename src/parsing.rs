044c4da7a7acfba043267eed5239c07eaa18638e
jewlexx <61040004+jewlexx@users.noreply.github.com>
2024-10-25 18:37:08 +1100

    implemented shim parsing

--- src/parsing.rs
+++ src/parsing.rs
@@ -16,20 +16,21 @@ pub enum Error {
 }
 
 pub fn parse_line(line: &str) -> Result<Option<(&str, &str)>, Error> {
+    // Allow for empty lines and comments
+    if line.is_empty() || line.starts_with('#') {
+        return Ok(None);
+    }
+
     let mut parts = line.splitn(2, '=');
 
-    let key = parts.next().ok_or(Error::MissingKey)?.trim();
-    let value = parts.next().ok_or(Error::MissingValue)?.trim();
+    let key = parse_possible_string(parts.next().ok_or(Error::MissingKey)?.trim());
+    let value = parse_possible_string(parts.next().ok_or(Error::MissingValue)?.trim());
 
     Ok(Some((key, value)))
 }
 
-fn parse_path(path: &str) -> PathBuf {
-    PathBuf::from(path)
-}
-
-fn parse_args(args: &str) -> Vec<String> {
-    args.split(' ').map(|s| s.to_string()).collect()
+fn parse_possible_string(key: &str) -> &str {
+    key.trim_matches('"')
 }
 
 impl FromStr for Shim {
@@ -56,5 +57,27 @@ impl FromStr for Shim {
         };
 
         Ok(Shim { path, args })
+    }
+}
+
+fn parse_path(path: &str) -> PathBuf {
+    PathBuf::from(path)
+}
+
+fn parse_args(args: &str) -> Vec<String> {
+    args.split(' ').map(|s| s.to_string()).collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_parse_possible_string() {
+        let quoted_string = parse_possible_string("\"path\"");
+        assert_eq!(quoted_string, "path");
+
+        let unquoted_string = parse_possible_string("path");
+        assert_eq!(unquoted_string, "path");
     }
 }
